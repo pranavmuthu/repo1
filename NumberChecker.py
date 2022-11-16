3a4a88f397a739574fb7f088835cf9d0e3803814
Pranav Muthuraman <pmuthura@uoregon.edu>
2022-11-16 12:14:16 -0800

    Changed periods to exclamation marks for more enthusiastic user experience

--- NumberChecker.py
+++ NumberChecker.py
@@ -6,9 +6,9 @@ num = int(num)
 if num > 1:
     for i in range(2, int(num/2)+1):
         if (num % i) == 0:
-            print(num, "is not a prime number.")
+            print(num, "is not a prime number!")
             break
     else:
-        print(num, "is a prime number.")
+        print(num, "is a prime number!")
 else:
-    print(num, "is not a prime number.")
+    print(num, "is not a prime number!")
