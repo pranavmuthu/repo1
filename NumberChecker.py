0ddd60b1c6c062bbc44204c45b258a458336890a
Pranav Muthuraman <pmuthura@uoregon.edu>
2022-11-16 12:31:32 -0800

    Added over_under functionality and added 3 exclamation marks

--- NumberChecker.py
+++ NumberChecker.py
@@ -3,12 +3,17 @@ import sys
 num = input("Please Enter a Number: ")
 num = int(num)
 
+if num > 100:
+    print(num, "is a large number!!!")
+else:
+    print(num, "is a small number!!!")
+
 if num > 1:
     for i in range(2, int(num/2)+1):
         if (num % i) == 0:
-            print(num, "is not a prime number!")
+            print(num, "is not a prime number!!!")
             break
     else:
-        print(num, "is a prime number!")
+        print(num, "is a prime number!!!")
 else:
-    print(num, "is not a prime number!")
+    print(num, "is not a prime number!!!")
