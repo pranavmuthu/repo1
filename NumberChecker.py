21e4095017db643849afe0b14c6a7ac85346599c
Pranav Muthuraman <pmuthura@uoregon.edu>
2022-11-16 12:29:16 -0800

    Added Odd_Even functionality

--- NumberChecker.py
+++ NumberChecker.py
@@ -3,6 +3,11 @@ import sys
 num = input("Please Enter a Number: ")
 num = int(num)
 
+if num % 2 == 0:
+    print(num, "is an even number!")
+else:
+    print(num, "is an odd number!")
+
 if num > 1:
     for i in range(2, int(num/2)+1):
         if (num % i) == 0:
